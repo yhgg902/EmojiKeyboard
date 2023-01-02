bd41c94ca17147f7826d235aa9e3bd6634533716
yhgaoFAGBAT <yg6952@utexas.edu>
2023-01-02 13:06:00 -0500

    initial commit

---
+++ EmojiCKeyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiCKeyboard
+//
+//  Created by YuhanGao on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
